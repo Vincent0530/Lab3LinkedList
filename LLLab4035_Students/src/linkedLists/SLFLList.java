b6f48769bbb7929275988a68443bc62cc49bfffd
4035d11 <4035d11@guama.ece.uprm.edu>
2018-03-12 17:31:00 -0400

    Segundo Commit

--- LLLab4035_Students/src/linkedLists/SLFLList.java
+++ LLLab4035_Students/src/linkedLists/SLFLList.java
@@ -10,6 +10,7 @@ package linkedLists;
 import java.util.NoSuchElementException;
 
 import linkedLists.LinkedList;
+import linkedLists.AbstractSLList.SNode;
 
 public class SLFLList<E> extends SLList<E>
 {
@@ -23,49 +24,100 @@ public class SLFLList<E> extends SLList<E>
 	
 	
 	public void addFirstNode(Node<E> nuevo) {
-		// TODO Auto-generated method stub
-		
+		SNode<E> sNuevo = (SNode<E>) nuevo;
+		if(length == 0){
+			first = sNuevo;
+			last = sNuevo;
+		}
+		sNuevo.setNext(first);
+		first = sNuevo;
+		length++;
 	}
 
 	public void addNodeAfter(Node<E> target, Node<E> nuevo) {
-		// TODO Auto-generated method stub
 		
+		// Pre: target is a node in the list
+		// Pre: nuevo is not a node in the list
+		((SNode<E>) nuevo).setNext(((SNode<E>) target).getNext()); 
+		((SNode<E>) target).setNext((SNode<E>) nuevo); 
+		length++; 
 	}
 
 	public void addNodeBefore(Node<E> target, Node<E> nuevo) {
-		// TODO Auto-generated method stub
+		if (target == first)
+			this.addFirstNode(nuevo); 
+		else { 
+			Node<E> prevNode = findNodePrevTo(target);  
+			this.addNodeAfter(prevNode, nuevo); 
+		}
 		
+	}
+	private Node<E> findNodePrevTo(Node<E> target) {
+		// Pre: target is a node in the list
+		if (target == first) 
+			return null; 
+		else { 
+			SNode<E> prev = first; 
+			while (prev != null && prev.getNext() != target) 
+				prev = prev.getNext();  
+			return prev; 
+		}
 	}
 
 	public Node<E> getFirstNode() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if (first == null)
+			throw new NoSuchElementException("getFirstNode() : linked list is empty..."); 
+		else{
+			return first;
+		}
 	}
 
 	public Node<E> getLastNode() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if (last == null)
+			throw new NoSuchElementException("getFirstNode() : linked list is empty..."); 
+		else{
+			return last;
+		}
 	}
 
 	public Node<E> getNodeAfter(Node<E> target) throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		
+		// Pre: target is a node in the list
+		SNode<E> aNode = ((SNode<E>) target).getNext(); 
+		if (aNode == null)  
+			throw new NoSuchElementException("getNextNode(...) : target is the last node."); 
+		else 
+			return aNode;
+		
 	}
 
 	public Node<E> getNodeBefore(Node<E> target)
 			throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		// Pre: target is a node in the list
+			if (target == first)  
+				throw new NoSuchElementException("getPrevNode(...) : target is the first node."); 
+			else 
+				return findNodePrevTo(target);
+		
 	}
 
 	public int length() {
 		// TODO Auto-generated method stub
-		return 0;
+		return length;
 	}
 
 	public void removeNode(Node<E> target) {
-		// TODO Auto-generated method stub
+		// Pre: target is a node in the list; hence, the list is not empty
 		
+		if (target == first) 
+			first = first.getNext(); 
+		else { 
+			SNode<E> prevNode = (SNode<E>) this.getNodeBefore(target); 
+					prevNode.setNext(((SNode<E>) target).getNext()); 
+		}
+		((SNode<E>) target).clean();   // clear all references from target
+		length--; 
 	}
 	
 	public Node<E> createNewNode() {
